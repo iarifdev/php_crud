9e94ef4e381343014771e121af970a759a2684d9
Muhammad Arif <talentedarif@gmail.com>
2020-12-08 21:57:39 +0500

    Login
    Login,
    Create session,
    and redirect post page

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,28 @@
                             <input type="submit" name="login" class="btn btn-primary" value="login" />
                         </form>
                         <!-- /Form  End -->
-                    </div>
+
+                    <?php
+                        if(isset($_POST['login'])){
+                            include "config.php";
+                            $username = mysqli_real_escape_string($conn, $_POST['username']);
+                            $password = md5($_POST['password']);
+                            $sql = "SELECT user_id, username, role FROM user WHERE username= '{$username}' AND password = '{$password}'";
+                            $result = mysqli_query($conn, $sql) or die("Query Failed.");
+                            if(mysqli_num_rows($result) > 0){
+                                while($row = mysqli_fetch_assoc($result)){
+                                    session_start();
+                                    $_SESSION['username'] = $row['username'];
+                                    $_SESSION['user_id'] = $row['user_id'];
+                                    $_SESSION['user_role'] = $row['role'];
+                                    header("Location: {$hostname}/admin/post.php");
+                                }
+                            }else{
+                                echo '<div class= "alert alert-danger">Username and Password Mismatch</div>';
+                            }      
+                        }
+                    ?>
+                   </div>
                 </div>
             </div>
         </div>
